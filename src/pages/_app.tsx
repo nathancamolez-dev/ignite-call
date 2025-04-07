a392c25b0ede5d0e2bf21050900c7a4696055c11
nathancamolez-dev <nathan.camolez@unesp.br>
2025-04-07 16:19:53 -0300

    feat: First configurations
    Cleaning the Next app, using old default Next application, without
    TurboPack and AppRouter. Using @ignite-ui for the Design System.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { globalStyle } from '@/styles/global'
+import type { AppProps } from 'next/app'
+
+globalStyle()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
