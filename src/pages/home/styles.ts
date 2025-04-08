5b7c26222591ecb6af685453ef04076859b6de13
nathancamolez-dev <nathan.camolez@unesp.br>
2025-04-08 16:35:56 -0300

    feat: Refactoring the Home style
    Center itens on the screen

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,7 +1,7 @@
 import { Heading, Text, styled } from '@ignite-ui/react'
 
 export const Container = styled('div', {
-  maxWidth: 'calc(100vw - (100vw - 1160vw) / 2)',
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
   height: '100vh',
   marginLeft: 'auto',
   display: 'flex',
