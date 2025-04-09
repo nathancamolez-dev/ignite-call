f6fe2acfec2bd7883734d3fb0da8274ed1ec6914
nathancamolez-dev <nathan.camolez@unesp.br>
2025-04-09 18:03:21 -0300

    feat: PrismaClient and route users
    Configuration of prisma and the first route to add a new user, on the
    SQLite db(SQLite for development)

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,3 +1,4 @@
+import { api } from '@/lib/axios'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
 import { useRouter } from 'next/router'
@@ -19,7 +20,14 @@ const registerFormSchema = z.object({
 type RegisterFormData = z.infer<typeof registerFormSchema>
 
 async function handleRegister(data: RegisterFormData) {
-  console.log(data)
+  try {
+    await api.post('/users', {
+      name: data.name,
+      username: data.username,
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 export default function Register() {
