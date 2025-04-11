dbf1e894a33e705b3021174eb10070b9dfc5ee26
nathancamolez-dev <nathan.camolez@unesp.br>
2025-04-10 21:53:13 -0300

    feat: scopes and permissions on google auth
    The scopes to ask if the user wants to give the permission to use the
    google calendar and profile information. If denied the page will be
    redirected to the connect-calendar with the error of permissions

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -7,8 +7,25 @@ export const authOptions: NextAuthOptions = {
     Google({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+        },
+      },
     }),
   ],
+  callbacks: {
+    async signIn({ account }) {
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/connect-calendar/?error=permissions'
+      }
+
+      return true
+    },
+  },
 }
 
 export default NextAuth(authOptions)
