1d265cd3eeb5ca3554a09480001e40441e415f8e
nathancamolez-dev <nathan.camolez@unesp.br>
2025-04-15 16:33:26 -0300

    feat: layout page time-intervals
    Start layout of the page time-intervals.

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,61 @@
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Container, Header } from '../styles'
+import {
+  IntervalBox,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+  IntervalsContainer,
+} from './styles'
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+      <IntervalBox as="form">
+        <IntervalsContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Segunda-Feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Terça-Feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+        </IntervalsContainer>
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  )
+}
